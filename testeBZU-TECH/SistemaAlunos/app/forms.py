ecb4f4761a402e029ed88a002a6360e7d60c978f
André Filipe <81587964+pluma01@users.noreply.github.com>
2021-11-23 21:33:25 -0300

    Delete testeBZU-TECH directory

--- testeBZU-TECH/SistemaAlunos/app/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from app.models import Alunos
-
-
-class Alunosform(ModelForm):
-    class Meta:
-        model = Alunos
-        fields = ['nome', 'cpf', 'avaliacao']
-        
