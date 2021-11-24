2a43d17496fa2f7872895b1f04e64d8913524b93
André Filipe <81587964+pluma01@users.noreply.github.com>
2021-11-23 21:32:07 -0300

    Delete CRUD_PYTHON directory

--- CRUD_PYTHON/APP/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from APP.models import Carros
-
-# Create the form class.
-class Carrosform(ModelForm):
-     class Meta:
-         model = Carros
-         fields = ['modelo', 'marca', 'ano']
--- CRUD_PYTHON/APP/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Alunos(models.Model):
-    nome = models.CharField(max_length=150)
-    cpf = models.IntegerField(max_length=100)
-    avaliacao = models.IntegerField(max_length=4)
